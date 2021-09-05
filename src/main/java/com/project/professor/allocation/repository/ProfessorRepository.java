b11e0ee8da353b542762858be3587d4978368578
walkiriatorres <85717908+walkiriatorres@users.noreply.github.com>
2021-09-04 23:23:36 -0300

    Adicionando query customizada findByCPF

--- src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
+++ src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
@@ -13,5 +13,7 @@ public interface ProfessorRepository extends JpaRepository<Professor, Long>{
 	List<Professor> findByNameContainingIgnoreCase(String name);
 	
 	List<Professor> findByDepartmentId(Long departmentId);
+	
+	Professor findByCPFContainingIgnoreCase(String cpf);
 
 }
